14c5afa066a525afd793236ad54e139c7df9580b
Ram Kumar D <quic_ramd@quicinc.com>
2025-01-30 14:37:48 +0530

    common: Move dyn memory init before fdtdec_setup
    The fdtdec_setup function requires the dynamic memory
    to be initialized before decompressing,
    especially when a compressed DTB blob is used.
    
    Therefore, the invocation of initf_malloc is
    moved before fdtdec_setup.
    
    Change-Id: Ia854e16811a68e41f60878ef21eefff88c21e4a0
    Signed-off-by: Ram Kumar D <quic_ramd@quicinc.com>

--- common/board_f.c
+++ common/board_f.c
@@ -7,6 +7,8 @@
  * (C) Copyright 2002
  * Sysgo Real-Time Solutions, GmbH <www.elinos.com>
  * Marius Groeger <mgroeger@sysgo.de>
+ *
+ * Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <config.h>
@@ -879,13 +881,13 @@ static int initf_upl(void)
 
 static const init_fnc_t init_sequence_f[] = {
 	setup_mon_len,
+	initf_malloc,
 #ifdef CONFIG_OF_CONTROL
 	fdtdec_setup,
 #endif
 #ifdef CONFIG_TRACE_EARLY
 	trace_early_init,
 #endif
-	initf_malloc,
 	initf_upl,
 	log_init,
 	initf_bootstage,	/* uses its own timer, so does not need DM */
