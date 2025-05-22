4354f0081771b6b6ba9bc7a9e5bafdd079be82ce
Vandhiadevan Karunamoorthy <vkarunam@qti.qualcomm.com>
2025-05-22 22:59:30 +0530

    net: AQ: Add 2.5Gbps and 5Gbps speed detection
    Change-Id: Ic00e40b8505fd4f9171594b43c59a3a9d471f1db
    Signed-off-by: Vandhiadevan Karunamoorthy <vkarunam@qti.qualcomm.com>

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -8,6 +8,7 @@
  *                                christopher.leech@intel.com,
  *                                scott.feldman@intel.com)
  * Portions Copyright (C) Sun Microsystems 2008
+ * Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef _LINUX_ETHTOOL_H
@@ -618,6 +619,7 @@ enum ethtool_sfeatures_retval_bits {
 #define SPEED_100		100
 #define SPEED_1000		1000
 #define SPEED_2500		2500
+#define SPEED_5000		5000
 #define SPEED_10000		10000
 #define SPEED_14000		14000
 #define SPEED_20000		20000
